fd44273b6ea6a6496c26410158385c5d3fd24f31
vicky208 <vicky208@DESKTOP-9884MIC.localdomain>
2022-11-24 11:59:16 -0800

    change

--- src/App.js
+++ src/App.js
@@ -5,15 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-title">
+          <img style="display:inline;" src= "ubcLogo.png" width="50px"></img>
+          <h1 className="App-name" style="display:inline;">Student Assistant Bot</h1>
+        </div>
       </header>
     </div>
   );
